f82a62a38fb332b5b9b55ea027d8211726610a6e
Aleksey <a.e.sokolkov@gmail.com>
2022-04-19 21:32:13 +0500

    500x500 saving, ClickOnImageToSave

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ image.src = 'EnglandFirstSlide.png';
 
 image.addEventListener('load', () => {
     ctx.drawImage(image,
-        x, y,       // Start at 70/20 pixels from the left and the top of the image (crop)
+        x, y,           // Start at 70/20 pixels from the left and the top of the image (crop)
         500, 500,       // "Get" a `50 * 50` (w * h) area from the source image (crop)
         0, 0,           // Place the result at 0, 0 in the canvas
         500, 500);      // With as width / height: 100 * 100 (scale)
 });
 
-function convert() {
+function submit() {
     const aDownloadLink = document.createElement('a');
     aDownloadLink.download = 'canvas_image.png';
     aDownloadLink.href = canvas.toDataURL();
