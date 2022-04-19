44fe6ece3f17d00c51e733acbdcc86aeafa9723c
Aleksey <a.e.sokolkov@gmail.com>
2022-04-19 22:19:56 +0500

    Own images, some animations

--- main.js
+++ main.js
@@ -1,40 +1,21 @@
-const image = new Image();
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
 
-let x = 500, y = 500;
+document.getElementById('input').onchange = function() {
+    document.getElementById('input-label').style.display = 'none';
+    canvas.style.display = 'flex';
+    const image = new Image();
+    image.onload = draw;
+    image.src = URL.createObjectURL(this.files[0]);
+};
 
-image.src = 'EnglandFirstSlide.png';
-
-image.addEventListener('load', () => {
-    ctx.drawImage(image,
-        x, y,           // Start at 70/20 pixels from the left and the top of the image (crop)
-        500, 500,       // "Get" a `50 * 50` (w * h) area from the source image (crop)
-        0, 0,           // Place the result at 0, 0 in the canvas
-        500, 500);      // With as width / height: 100 * 100 (scale)
-});
+function draw() {
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(this, 0, 0);
+}
 
 function submit() {
-    const aDownloadLink = document.createElement('a');
-    aDownloadLink.download = 'canvas_image.png';
-    aDownloadLink.href = canvas.toDataURL();
-    aDownloadLink.click();
-}
-
-function moveLeft() {
-    x--;
-    ctx.drawImage(image,
-        x, y,
-        500, 500,
-        0, 0,
-        500, 500);
-}
-
-function moveRight() {
-    x++;
-    ctx.drawImage(image,
-        x, y,
-        500, 500,
-        0, 0,
-        500, 500);
+    const link = document.createElement('a');
+    link.download = 'canvas_image.png';
+    link.href = canvas.toDataURL();
+    link.click();
 }
