e7cdc94c96b6645f919a33338946a078a22cc5a2
Aleksey <a.e.sokolkov@gmail.com>
2022-04-19 20:02:15 +0500

    Cropping, saving, moving

--- main.js
+++ main.js
@@ -1,77 +1,40 @@
-function convertor(imageFileBlob, options) {
+const image = new Image();
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-    const settings = extend({}, options || {});
+let x = 500, y = 500;
 
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext("2d");
-    const imageEl = createImage();
-    const downloadLink = settings.downloadEl || createDownloadLink();
+image.src = 'EnglandFirstSlide.png';
 
-    function createImage(options) {
-        options = options || {};
-        const img = (Image) ? new Image() : document.createElement('img');
-        img.style.width = (options.width) ? options.width + 'px' : 'auto';
-        img.style.height = (options.height) ? options.height + 'px' : 'auto';
-        return img;
-    }
+image.addEventListener('load', () => {
+    ctx.drawImage(image,
+        x, y,       // Start at 70/20 pixels from the left and the top of the image (crop)
+        500, 500,       // "Get" a `50 * 50` (w * h) area from the source image (crop)
+        0, 0,           // Place the result at 0, 0 in the canvas
+        500, 500);      // With as width / height: 100 * 100 (scale)
+});
 
-    function extend(target, source) {
-        for (let propName in source)
-            if (source.hasOwnProperty(propName))
-                target[propName] = source[propName];
-        return target;
-    }
-
-    function createDownloadLink() {
-        return document.createElement('a');
-    }
-
-    function download() {
-        if ('click' in downloadLink)
-            downloadLink.click();
-        else
-            downloadLink.dispatchEvent(createClickEvent());
-    }
-
-    function updateDownloadLink(jpgFileName, pngBlob) {
-        const linkEl = downloadLink;
-        const pngFileName = jpgFileName.replace(/jpe?g/i, 'png');
-
-        linkEl.setAttribute('download', pngFileName);
-        linkEl.href = window.URL.createObjectURL(pngBlob);
-
-        if (settings.downloadEl)
-            settings.downloadEl.style.display = 'block';
-        else
-            download();
-    }
-
-    function createClickEvent() {
-        if ('MouseEvent' in window)
-            return new MouseEvent('click');
-        else
-            return MouseEvent.initMouseEvent('click', true, true, window);
-    }
-
-    const imageUrl = window.URL.createObjectURL(imageFileBlob);
-
-    imageEl.onload = (e) => {
-        canvas.width = e.target.width;
-        canvas.height = e.target.height;
-        ctx.drawImage(e.target, 0, 0, e.target.width, e.target.height);
-        canvas.toBlob(updateDownloadLink.bind(window, imageFileBlob.name), 'image/png', 1);
-    };
-
-    imageEl.src = imageUrl;
-    if (settings.downloadEl)
-        settings.downloadEl.style.display = 'none';
+function convert() {
+    const aDownloadLink = document.createElement('a');
+    aDownloadLink.download = 'canvas_image.png';
+    aDownloadLink.href = canvas.toDataURL();
+    aDownloadLink.click();
 }
 
-document.querySelector('[name="image"]')
-    .addEventListener('change', (event) => {
-    const jpgImageFileBlob = event.currentTarget.files[0];
-    if (jpgImageFileBlob.type.match(/image\/jpe?g/i) !== null)
-        convertor(jpgImageFileBlob);
-    else
-        alert('Invalid JPG image file');
-}, false);
+function moveLeft() {
+    x--;
+    ctx.drawImage(image,
+        x, y,
+        500, 500,
+        0, 0,
+        500, 500);
+}
+
+function moveRight() {
+    x++;
+    ctx.drawImage(image,
+        x, y,
+        500, 500,
+        0, 0,
+        500, 500);
+}
