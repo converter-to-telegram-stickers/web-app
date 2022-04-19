97434ec148a9294b48c76c12aa0ea2b22d75a13e
Aleksey <a.e.sokolkov@gmail.com>
2022-04-20 00:14:25 +0500

    Simpler Code 2

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const canvas = document.getElementById('canvas');
+const input = document.getElementById('input');
+const inputLabel = document.getElementById('input-label');
+const image = new Image();
+const ctx = canvas.getContext('2d');
 
-document.getElementById('input').onchange = function() {
-    document.getElementById('input-label').style.display = 'none';
+input.onchange = function() {
+    inputLabel.style.display = 'none';
     canvas.style.display = 'flex';
-    const image = new Image();
+    const imageFile = this.files[0];
     image.onload = draw;
-    image.src = URL.createObjectURL(this.files[0]);
+    image.src = URL.createObjectURL(imageFile);
 };
 
 function draw() {
-    const ctx = canvas.getContext('2d');
     ctx.drawImage(this, 0, 0);
 }
 
 function submit() {
     const link = document.createElement('a');
-    link.download = 'canvas_image.png';
+    link.download = 'image.png';
     link.href = canvas.toDataURL();
     link.click();
 }
