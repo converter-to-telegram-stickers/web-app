3e4719c9b8001084be8b809a456dabecadf745be
Aleksey <a.e.sokolkov@gmail.com>
2022-04-20 16:37:21 +0500

    Image dragging

--- main.js
+++ main.js
@@ -22,3 +22,46 @@ function submit() {
     link.href = canvas.toDataURL();
     link.click();
 }
+
+let moveXAmount = 0;
+let moveYAmount = 0;
+let dragging = false;
+let prevX = 0;
+let prevY = 0;
+
+function make_pic() {
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.save();
+
+    ctx.translate(200, 275); // зачем
+    ctx.drawImage(image, -500 / 2 + moveXAmount, -500 / 2 + moveYAmount, image.width, image.height);
+    ctx.restore();
+}
+
+canvas.onmousedown = function() {
+    dragging = true;
+    prevX = 0;
+    prevY = 0;
+}
+
+window.onmouseup = function() {
+    dragging = false;
+    prevX = 0;
+    prevY = 0;
+}
+
+window.onmousemove = function(e) {
+    if (dragging)
+    {
+        if (prevX > 0 || prevY > 0)
+        {
+            moveXAmount += e.pageX - prevX;
+            moveYAmount += e.pageY - prevY;
+            make_pic();
+        }
+        prevX = e.pageX;
+        prevY = e.pageY;
+    }
+}
+
+make_pic();
